65554e712319d0c7dc28954155842db157dbf4b9
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-02-03 18:56:19 +0000

    Add files via upload

--- class_circle.py
+++ class_circle.py
@@ -2,22 +2,31 @@ import pgzrun
 
 HEIGHT=500
 WIDTH=500
+gravity=2000
 
 class circles():
-    def __init__(self,color,radius,pos):
+    def __init__(self,color,radius,x,y):
        self.color=color
        self.radius=radius
-       self.pos=pos
+       self.pos=(x,y)
+       self.x=x
+       self.y=y
+       self.vx=200
+       self.vy=0
 
     def drawcircle(self):
         screen.draw.filled_circle(self.pos,self.radius,self.color)
 
-circle1=circles(("blue"),150,(150,150))
-circle2=circles(("red"),50,(50,50))
 
+circle2=circles(("red"),50,50,50)
 
 def draw():
-    circle1.drawcircle()
     circle2.drawcircle()
+
+def update(dt):
+    uy=circle2.vy
+    
+
+
 
 pgzrun.go()
