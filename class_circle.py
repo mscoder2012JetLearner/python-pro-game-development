d9f53a2a91adb9de49f97568f024969908a3d4da
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-02-10 18:45:09 +0000

    Add files via upload

--- class_circle.py
+++ class_circle.py
@@ -3,6 +3,8 @@ import pgzrun
 HEIGHT=500
 WIDTH=500
 gravity=2000
+import os
+os.environ['SDL_VIDEO_CENTERED']='1'
 
 class circles():
     def __init__(self,color,radius,x,y):
@@ -15,6 +17,7 @@ class circles():
        self.vy=0
 
     def drawcircle(self):
+        self.pos=(self.x,self.y)
         screen.draw.filled_circle(self.pos,self.radius,self.color)
 
 
@@ -29,6 +32,13 @@ def update(dt):
     uy=circle2.vy
     circle2.vy=circle2.vy+gravity*dt
     circle2.y=circle2.y+(uy+circle2.vy)*0.5*dt
-    print(circle2.y)
+    if circle2.y>500:
+        circle2.y=500-circle2.radius
+        circle2.vy=-circle2.vy*0.9
+    circle2.x=circle2.x+circle2.vx*dt
+    if circle2.x>500 or circle2.x<0:
+        circle2.vx=-circle2.vx*0.9
+        
+
 
 pgzrun.go()
