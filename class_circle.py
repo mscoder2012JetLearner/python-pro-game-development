90fc1e1291b26fd59bf53738edc99f9edf0c6713
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-02-10 18:22:16 +0000

    Add files via upload

--- class_circle.py
+++ class_circle.py
@@ -21,12 +21,14 @@ class circles():
 circle2=circles(("red"),50,50,50)
 
 def draw():
+    screen.clear()
     circle2.drawcircle()
+    
 
 def update(dt):
     uy=circle2.vy
-    
-
-
+    circle2.vy=circle2.vy+gravity*dt
+    circle2.y=circle2.y+(uy+circle2.vy)*0.5*dt
+    print(circle2.y)
 
 pgzrun.go()
