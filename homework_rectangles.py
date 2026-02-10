818d3491cc3314818a1fd859860c7b8ac081687e
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-02-10 18:05:24 +0000

    Add files via upload

---
+++ homework_rectangles.py
@@ -0,0 +1,31 @@
+import pygame
+pygame.init()
+
+sc=pygame.display.set_mode((500,500))
+pygame.display.set_caption("Output")
+sc.fill("black")
+pygame.display.update()
+
+class rectangle():
+    
+    def __init__(self,color,dimension):
+        self.color=color
+        self.dimension=dimension
+
+    def draw(self):
+        pygame.draw.rect(sc,self.color,self.dimension,10)
+
+rectangle1=rectangle("green",(10,150,250,150))
+rectangle1.draw()
+rectangle2=rectangle("purple",(200,230,350,250))
+rectangle2.draw()
+pygame.display.update()
+
+        
+    
+
+while True:
+    for i in pygame.event.get():
+        if i.type==pygame.QUIT:
+            pygame.quit()
+
