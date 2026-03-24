9e5c0bf1cf67b2941e7520d21b50a1cbc56e3aaa
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-03-24 18:56:39 +0000

    Add files via upload

---
+++ recycle_marathon.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+sc=pygame.display.set_mode((900,600))
+
+class bin(pygame.sprite.Sprite)
+
+while True:
+     for i in pygame.event.get():
+        if i.type==pygame.QUIT:
+            pygame.quit()
