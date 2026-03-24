d2089f8b1f1660212f7bfb1518b75e7c3aa3fa3c
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-03-24 18:57:19 +0000

    Add files via upload

--- bee_game.py
+++ bee_game.py
@@ -1,9 +1,10 @@
 import pygame
 
+framespersecond=700
+clock=pygame.time.Clock()
 pygame.init()
 sc=pygame.display.set_mode((500,500))
 pygame.display.set_caption("bee game")
-sc.fill("white")
 pygame.display.update()
 
 game_over=False
@@ -11,25 +12,30 @@ score=0
 
 bee=pygame.image.load("C:/Users/31mschwarz/OneDrive - Abbey Gate College/Documents and subjects/python game developer/images/bee.png")
 
+bee_rect=pygame.Rect(50,50,50,50)
+
 def draw(bee):
-    sc.blit(bee,(50,50))
+    sc.fill("white")
+    sc.blit(bee,(bee_rect.x,bee_rect.y))
     pygame.display.update()
 
 
-def movebee(keypress,bee):
+def movebee(keypress,bee_rect):
     if keypress[pygame.K_LEFT]:
-          bee.x=bee.x-1
+          bee_rect.x=bee_rect.x-1
     if keypress[pygame.K_RIGHT]:
-          bee.x=bee.x+1
+          bee_rect.x=bee_rect.x+1
     if keypress[pygame.K_UP]:
-          bee.y=bee.y-1
+          bee_rect.y=bee_rect.y-1
     if keypress[pygame.K_DOWN]:
-          bee.y=bee.y+1
+          bee_rect.y=bee_rect.y+1
 draw(bee)
 
 while True:
+    clock.tick(framespersecond)
     keypress=pygame.key.get_pressed()
-    movebee(keypress,bee)
+    movebee(keypress,bee_rect)
+    draw(bee)
     for i in pygame.event.get():
         if i.type==pygame.QUIT:
             pygame.quit()
