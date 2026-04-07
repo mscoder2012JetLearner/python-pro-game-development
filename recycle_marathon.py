b435d547c813ce4f8fb7157708644fa0e59b4075
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-04-07 18:54:48 +0100

    Add files via upload

--- recycle_marathon.py
+++ recycle_marathon.py
@@ -2,8 +2,22 @@ import pygame
 pygame.init()
 
 sc=pygame.display.set_mode((900,600))
+background=pygame.image.load(r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\recycle_bg.png")
+background=pygame.transform.scale(background,(900,600))
+sc.blit(background,(0,0))
+pygame.display.update()
 
-class bin(pygame.sprite.Sprite)
+class bin(pygame.sprite.Sprite):
+    def __init__(self):
+
+        super().__init__()
+        self.image=pygame.image.load(r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\bin.png")
+        self.image=pygame.transform.scale(self.image,(40,60))
+        self.rect=self.image.get_rect()
+        
+    
+bin1=bin()
+
 
 while True:
      for i in pygame.event.get():
