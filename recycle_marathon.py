c060f405e9219df280e7c2008ee4b4dcb556d469
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-04-21 18:57:08 +0100

    Add files via upload

--- recycle_marathon.py
+++ recycle_marathon.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 sc=pygame.display.set_mode((900,600))
@@ -14,12 +15,32 @@ class bin(pygame.sprite.Sprite):
         self.image=pygame.image.load(r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\bin.png")
         self.image=pygame.transform.scale(self.image,(40,60))
         self.rect=self.image.get_rect()
-        
-    
+
+recycleable_images=[r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\paper bag.png",r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\pencil.png",r"C:\Users\31mschwarz\OneDrive - Abbey Gate College\python pro game developer\images\wooden box.png"]
+class recycleable(pygame.sprite.Sprite):
+    def __init__(self):
+
+        super().__init__()
+        self.image=pygame.image.load(random.choice(recycleable_images))
+        self.image=pygame.transform.scale(self.image,(30,50))
+        self.rect=self.image.get_rect()
+
+
+all_items=pygame.sprite.Group()
 bin1=bin()
+for i in range(40):
+    x=random.randint(20,880)
+    y=random.randint(20,580)
+    r=recycleable()
+    r.rect.x=x
+    r.rect.y=y
+    all_items.add(r)
+all_items.add(bin1)
 
 
 while True:
+     all_items.draw(sc)
+     pygame.display.update()
      for i in pygame.event.get():
         if i.type==pygame.QUIT:
             pygame.quit()
